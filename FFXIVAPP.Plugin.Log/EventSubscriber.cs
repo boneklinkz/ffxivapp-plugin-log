28bd9af9f48222fd9be448c7ae714cbd4c88749f
Ryan Wilson <syndicated.life@gmail.com>
2014-01-02 13:49:49 -0800

    commented out unused events and updated for new common.dll

--- FFXIVAPP.Plugin.Log/EventSubscriber.cs
+++ FFXIVAPP.Plugin.Log/EventSubscriber.cs
@@ -15,26 +15,26 @@ namespace FFXIVAPP.Plugin.Log
         {
             Plugin.PHost.NewConstantsEntity += OnNewConstantsEntity;
             Plugin.PHost.NewChatLogEntry += OnNewChatLogEntry;
-            Plugin.PHost.NewMonsterEntries += OnNewMonsterEntries;
-            Plugin.PHost.NewNPCEntries += OnNewNPCEntries;
-            Plugin.PHost.NewPCEntries += OnNewPCEntries;
-            Plugin.PHost.NewPlayerEntity += OnNewPlayerEntity;
-            Plugin.PHost.NewTargetEntity += OnNewTargetEntity;
-            Plugin.PHost.NewParseEntity += OnNewParseEntity;
-            Plugin.PHost.NewPartyEntries += OnNewPartyEntries;
+            //Plugin.PHost.NewMonsterEntries += OnNewMonsterEntries;
+            //Plugin.PHost.NewNPCEntries += OnNewNPCEntries;
+            //Plugin.PHost.NewPCEntries += OnNewPCEntries;
+            //Plugin.PHost.NewPlayerEntity += OnNewPlayerEntity;
+            //Plugin.PHost.NewTargetEntity += OnNewTargetEntity;
+            //Plugin.PHost.NewParseEntity += OnNewParseEntity;
+            //Plugin.PHost.NewPartyEntries += OnNewPartyEntries;
         }
 
         public static void UnSubscribe()
         {
             Plugin.PHost.NewConstantsEntity -= OnNewConstantsEntity;
             Plugin.PHost.NewChatLogEntry -= OnNewChatLogEntry;
-            Plugin.PHost.NewMonsterEntries -= OnNewMonsterEntries;
-            Plugin.PHost.NewNPCEntries -= OnNewNPCEntries;
-            Plugin.PHost.NewPCEntries -= OnNewPCEntries;
-            Plugin.PHost.NewPlayerEntity -= OnNewPlayerEntity;
-            Plugin.PHost.NewTargetEntity -= OnNewTargetEntity;
-            Plugin.PHost.NewParseEntity -= OnNewParseEntity;
-            Plugin.PHost.NewPartyEntries -= OnNewPartyEntries;
+            //Plugin.PHost.NewMonsterEntries -= OnNewMonsterEntries;
+            //Plugin.PHost.NewNPCEntries -= OnNewNPCEntries;
+            //Plugin.PHost.NewPCEntries -= OnNewPCEntries;
+            //Plugin.PHost.NewPlayerEntity -= OnNewPlayerEntity;
+            //Plugin.PHost.NewTargetEntity -= OnNewTargetEntity;
+            //Plugin.PHost.NewParseEntity -= OnNewParseEntity;
+            //Plugin.PHost.NewPartyEntries -= OnNewPartyEntries;
         }
 
         #region Subscriptions
@@ -74,94 +74,93 @@ namespace FFXIVAPP.Plugin.Log
             }
             catch (Exception ex)
             {
-                //Logging.Log(LogManager.GetCurrentClassLogger(), "", ex);
             }
         }
 
-        private static void OnNewMonsterEntries(object sender, ActorEntitiesEvent actorEntitiesEvent)
-        {
-            // delegate event from monster entities from ram, not required to subsribe
-            // this updates 10x a second and only sends data if the items are found in ram
-            // currently there no change/new/removed event handling (looking into it)
-            if (sender == null)
-            {
-                return;
-            }
-            var monsterEntities = actorEntitiesEvent.ActorEntities;
-        }
+        //private static void OnNewMonsterEntries(object sender, ActorEntitiesEvent actorEntitiesEvent)
+        //{
+        //    // delegate event from monster entities from ram, not required to subsribe
+        //    // this updates 10x a second and only sends data if the items are found in ram
+        //    // currently there no change/new/removed event handling (looking into it)
+        //    if (sender == null)
+        //    {
+        //        return;
+        //    }
+        //    var monsterEntities = actorEntitiesEvent.ActorEntities;
+        //}
 
-        private static void OnNewNPCEntries(object sender, ActorEntitiesEvent actorEntitiesEvent)
-        {
-            // delegate event from npc entities from ram, not required to subsribe
-            // this list includes anything that is not a player or monster
-            // this updates 10x a second and only sends data if the items are found in ram
-            // currently there no change/new/removed event handling (looking into it)
-            if (sender == null)
-            {
-                return;
-            }
-            var npcEntities = actorEntitiesEvent.ActorEntities;
-        }
+        //private static void OnNewNPCEntries(object sender, ActorEntitiesEvent actorEntitiesEvent)
+        //{
+        //    // delegate event from npc entities from ram, not required to subsribe
+        //    // this list includes anything that is not a player or monster
+        //    // this updates 10x a second and only sends data if the items are found in ram
+        //    // currently there no change/new/removed event handling (looking into it)
+        //    if (sender == null)
+        //    {
+        //        return;
+        //    }
+        //    var npcEntities = actorEntitiesEvent.ActorEntities;
+        //}
 
-        private static void OnNewPCEntries(object sender, ActorEntitiesEvent actorEntitiesEvent)
-        {
-            // delegate event from player entities from ram, not required to subsribe
-            // this updates 10x a second and only sends data if the items are found in ram
-            // currently there no change/new/removed event handling (looking into it)
-            if (sender == null)
-            {
-                return;
-            }
-            var pcEntities = actorEntitiesEvent.ActorEntities;
-        }
+        //private static void OnNewPCEntries(object sender, ActorEntitiesEvent actorEntitiesEvent)
+        //{
+        //    // delegate event from player entities from ram, not required to subsribe
+        //    // this updates 10x a second and only sends data if the items are found in ram
+        //    // currently there no change/new/removed event handling (looking into it)
+        //    if (sender == null)
+        //    {
+        //        return;
+        //    }
+        //    var pcEntities = actorEntitiesEvent.ActorEntities;
+        //}
 
-        private static void OnNewPlayerEntity(object sender, PlayerEntityEvent playerEntityEvent)
-        {
-            // delegate event from player info from ram, not required to subsribe
-            // this is for YOU and includes all your stats and your agro list with hate values as %
-            // this updates 10x a second and only sends data when the newly read data is differen than what was previously sent
-            if (sender == null)
-            {
-                return;
-            }
-            var playerEntity = playerEntityEvent.PlayerEntity;
-        }
+        //private static void OnNewPlayerEntity(object sender, PlayerEntityEvent playerEntityEvent)
+        //{
+        //    // delegate event from player info from ram, not required to subsribe
+        //    // this is for YOU and includes all your stats and your agro list with hate values as %
+        //    // this updates 10x a second and only sends data when the newly read data is differen than what was previously sent
+        //    if (sender == null)
+        //    {
+        //        return;
+        //    }
+        //    var playerEntity = playerEntityEvent.PlayerEntity;
+        //}
 
-        private static void OnNewTargetEntity(object sender, TargetEntityEvent targetEntityEvent)
-        {
-            // delegate event from target info from ram, not required to subsribe
-            // this includes the full entities for current, previous, mouseover, focus targets (if 0+ are found)
-            // it also includes a list of upto 16 targets that currently have hate on the currently targeted monster
-            // these hate values are realtime and change based on the action used
-            // this updates 10x a second and only sends data when the newly read data is differen than what was previously sent
-            if (sender == null)
-            {
-                return;
-            }
-            var targetEntity = targetEntityEvent.TargetEntity;
-        }
+        //private static void OnNewTargetEntity(object sender, TargetEntityEvent targetEntityEvent)
+        //{
+        //    // delegate event from target info from ram, not required to subsribe
+        //    // this includes the full entities for current, previous, mouseover, focus targets (if 0+ are found)
+        //    // it also includes a list of upto 16 targets that currently have hate on the currently targeted monster
+        //    // these hate values are realtime and change based on the action used
+        //    // this updates 10x a second and only sends data when the newly read data is differen than what was previously sent
+        //    if (sender == null)
+        //    {
+        //        return;
+        //    }
+        //    var targetEntity = targetEntityEvent.TargetEntity;
+        //}
 
-        private static void OnNewParseEntity(object sender, ParseEntityEvent parseEntityEvent)
-        {
-            // delegate event from data work; which right now has basic parsing stats for widgets.
-            // includes global total stats for damage, healing, damage taken
-            // include player list with name, hps, dps, dtps, total stats like the global and percent of each total stat
-            if (sender == null)
-            {
-                return;
-            }
-            var parseEntity = parseEntityEvent.ParseEntity;
-        }
+        //private static void OnNewParseEntity(object sender, ParseEntityEvent parseEntityEvent)
+        //{
+        //    // delegate event from data work; which right now has basic parsing stats for widgets.
+        //    // includes global total stats for damage, healing, damage taken
+        //    // include player list with name, hps, dps, dtps, total stats like the global and percent of each total stat
+        //    if (sender == null)
+        //    {
+        //        return;
+        //    }
+        //    var parseEntity = parseEntityEvent.ParseEntity;
+        //}
 
-        private static void OnNewPartyEntries(object sender, PartyEntitiesEvent partyEntitiesEvent)
-        {
-            // delegate event that shows current party basic info
-            if (sender == null)
-            {
-                return;
-            }
-            var partyEntities = partyEntitiesEvent.PartyEntities;
-        }
+        //private static void OnNewPartyEntries(object sender, PartyEntitiesEvent partyEntitiesEvent)
+        //{
+        //    // delegate event that shows current party basic info
+        //    if (sender == null)
+        //    {
+        //        return;
+        //    }
+        //    var partyEntities = partyEntitiesEvent.PartyEntities;
+        //}
 
         #endregion
     }
